c3caa89b633f71949f5c6a898ecba1ee42fbcf30
alanbanks229 <alanbanks229@gmail.com>
2020-08-23 13:58:37 -0400

    added additional comments for routes and conventions

---
+++ app/controllers/exercise_controller.rb
@@ -0,0 +1,8 @@
+class ExerciseController < ApplicationController
+
+    def index
+        render "index"
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   # Implementing a static route
   # to access we go to localhost:3000/about
   # make sure we have a controller 'static' with a method "about"
+  # name of view folder needs to match 
+  # "xyz" where "xyz"_controller
   get 'about', to: 'static#about'
+
+  # this allows route /exercises
+  get 'exercises', to: 'exercise#index'
+
+  # this allows route /exercise
+  resources :exercise, only: [:index]
 end
