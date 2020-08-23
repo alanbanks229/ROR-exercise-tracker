bccb645491c85e06948612371b3fd66a7a7f9606
alanbanks229 <alanbanks229@gmail.com>
2020-08-23 13:28:13 -0400

    implemented about route, inside static_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Implementing a static route
+  # to access we go to localhost:3000/about
+  # make sure we have a controller 'static' with a method "about"
+  get 'about', to: 'static#about'
 end
